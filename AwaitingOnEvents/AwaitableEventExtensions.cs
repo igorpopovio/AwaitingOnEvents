45b3f7ab497542fd215cac4222eb8895b48e7953
Igor Popov <igor@igorpopov.io>
2018-06-17 11:05:36 +0300

    Add extension method to await on button.Click
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ AwaitingOnEvents/AwaitableEventExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AwaitingOnEvents
+{
+    public static class AwaitableEventExtensions
+    {
+        public static async Task ClickAsync(this Button button)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            RoutedEventHandler handler = (s, e) =>
+            {
+                tcs.TrySetResult(null);
+            };
+
+            try
+            {
+                button.Click += handler;
+                await tcs.Task;
+            }
+            finally
+            {
+                button.Click -= handler;
+            }
+        }
+    }
+}
